468061e0a71c0b86c56d22b15ad2ec974b9ef0ad
Ryan Parks <rjp@ryans-mbp.attlocal.net>
2018-02-26 21:02:57 -0600

    pages linked. survey question data pushes to the api friends list. minor formating changes and typos corrected on both html pages

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friendList = [
+    {
+        name: "Ryan",
+        photo: "https://thumb9.shutterstock.com/mosaic_250/698308/567772042/stock-photo-headshot-of-successful-smiling-cheerful-african-american-businessman-executive-stylish-company-567772042.jpg",
+        scores: [
+            5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+        ]
+    },
+    {
+        name: "Katie",
+        photo: "https://i.pinimg.com/236x/6e/f7/ca/6ef7ca39190bfb053f43e1ea3b03f7a7--corporate-portrait-corporate-headshots.jpg",
+        scores: [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+        ]
+    },
+    {
+        name: "Avi",
+        photo: "https://static9.depositphotos.com/1549192/1100/i/950/depositphotos_11000778-stock-photo-hispanic-male-headshot.jpg",
+        scores: [
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2
+        ]
+    },
+    {
+        name: "Erica",
+        photo: "https://media.istockphoto.com/photos/business-woman-headshot-portrait-picture-id465479132?k=6&m=465479132&s=612x612&w=0&h=m_LQAGelTnNQs5lqdeox4fJq9kN9LveLsZiZXb3_LfU=",
+        scores: [
+            3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+        ]
+    },
+    {
+        name: "Shawn",
+        photo: "https://i.pinimg.com/236x/8d/23/5f/8d235f1603100bc5ab4e78de36e1b110--mens-headshots-young-man.jpg",
+        scores: [
+            4, 4, 4, 4, 4, 4, 4, 4, 4, 4
+        ]
+    }
+];
+
+module.exports = friendList;
