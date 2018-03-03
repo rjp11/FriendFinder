d883336e15f4a1e4374d4d9d9831170022df3564
Ryan Parks <ryan.j.parks12@gmail.com>
2018-03-03 16:12:18 -0600

    friend match logic moved to apiRoutes.js file. Friend match still appended to bottom of survey html. need to add modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
 var friendData = require("../data/friends");
+var bestFriend;
 
 
 module.exports = function (app) {
@@ -8,9 +9,42 @@ module.exports = function (app) {
   });
 
   app.post("/api/friendsList", function (req, res) {
+
     friendData.push(req.body);
-    res.json(true);
+
+    findFriend();
+
+    res.json(bestFriend);
 
   });
 
+};
+
+function findFriend() {
+
+  // initialize score value to check the difference against
+  var score = 50;
+  var totalFriends = friendData.length - 1;
+
+
+  // double for loop 
+  // first one to iterate through all of the friends data and 
+  // the second to iterate through the responses of each of the friends
+  // use (res.length - 1) so it doesn't say you are your own best friend
+  for (var i = 0; i < totalFriends; i++) {
+    var diffArray = [];
+
+    for (var j = 0; j < 10; j++) {
+      diffArray[j] = Math.abs(friendData[i].scores[j] - friendData[totalFriends].scores[j]);
+    }
+    const reducer = (accumulator, currentValue) => accumulator +
+      currentValue;
+    var scoreCheck = diffArray.reduce(reducer);
+    console.log(scoreCheck);
+
+    if (scoreCheck < score) {
+      bestFriend = friendData[i];
+      score = scoreCheck;
+    };
+  };
 };
